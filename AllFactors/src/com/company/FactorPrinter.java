761aad1f50cc957a07179455f783542976a2fcef
handywts <38926106+handywts@users.noreply.github.com>
2019-10-16 07:42:24 +0200

    Add files via upload

---
+++ AllFactors/src/com/company/FactorPrinter.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class FactorPrinter {
+
+    public static void printFactors(int number) {
+        if(number < 1) {
+            System.out.println("Invalid Value");
+        }
+
+        for(int i = 1; i < number+1; i++) {
+            if(number%i == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
